207a48f01389872cf3abfd140a1f4a828cdea4db
Samuel Quaresma <squoliver@gmail.com>
2023-10-09 18:33:21 -0300

    User CRUD

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dscatalog.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,9 +24,10 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String email;
     private String password;
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "tb_user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))

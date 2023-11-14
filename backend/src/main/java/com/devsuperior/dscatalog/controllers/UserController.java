46f206437285d5aeeabd5707bc83d2e20cf88b57
Samuel Quaresma <squoliver@gmail.com>
2023-11-14 12:52:26 -0300

    Signup implementado

--- backend/src/main/java/com/devsuperior/dscatalog/controllers/UserController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/UserController.java
@@ -43,7 +43,6 @@ public class UserController {
         return ResponseEntity.ok().body(dto);
     }
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserInsertDTO dto) {
         UserDTO newDto = service.insert(dto);
--- backend/src/main/java/com/devsuperior/dscatalog/repositories/RoleRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/RoleRepository.java
@@ -4,4 +4,6 @@ import com.devsuperior.dscatalog.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByAuthority(String authority);
 }
